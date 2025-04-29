6b29b2432dbc9e7fbe74f6646eb7a4b839c41d84
Avan Singh <avansingh085@gmail.com>
2025-04-30 00:37:11 +0530

    first commit

---
+++ backend/src/main/java/com/avan/backend/StreamingPlatformApplicationa.java
@@ -0,0 +1,14 @@
+package com.avan.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamingPlatformApplicationa {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamingPlatformApplicationa.class, args);
+		System.out.println("HELLOWWW");
+	}
+
+}
